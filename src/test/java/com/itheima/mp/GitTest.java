126f2d8e850c2b90350f39077972d0a7af53a4eb
seven <seven@guigu.com>
2025-03-18 19:12:16 +0800

    second commit

--- src/test/java/com/itheima/mp/GitTest.java
+++ src/test/java/com/itheima/mp/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("154545");
         System.out.println("54545");
+        System.out.println("4546544");
     }
 }
