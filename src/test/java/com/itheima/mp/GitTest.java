5d5e919f248117f723e6b6fe58bb5b222ec2d2c3
seven <seven@guigu.com>
2025-03-18 19:10:08 +0800

    first commit

---
+++ src/test/java/com/itheima/mp/GitTest.java
@@ -0,0 +1,8 @@
+package com.itheima.mp;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("154545");
+        System.out.println("54545");
+    }
+}
